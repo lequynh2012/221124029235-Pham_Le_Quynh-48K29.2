2d94cbbc101aca232e11615dd27582c475b92b29
phamlequynh <quynhletkdn@gmail.com>
2025-03-19 18:18:14 +0700

    welcome.bat

--- welcome.bat
+++ welcome.bat
@@ -3,6 +3,3 @@ echo Starting local server at http://localhost:8080
 start http://localhost:8080/menu.html
 python -m http.server 8080
 pause
-
-git config --global user.name "phamlequynh"
-git config --global user.email "quynhletkdn@gmail.com"
